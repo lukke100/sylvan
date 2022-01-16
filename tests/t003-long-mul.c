49c877a42688e1ad7d628b3119c43b30a96a85a6
Lucas Hart <lukke100@gmail.com>
2022-01-16 12:10:52 -0500

    Improve the tests for long multiplication

--- tests/t003-long-mul.c
+++ tests/t003-long-mul.c
@@ -9,18 +9,45 @@ int main(void)
 
 	assert(sy_lmul(LONG_MAX, 2, NULL) == LONG_MAX);
 	assert(sy_lmul(LONG_MIN, 2, NULL) == LONG_MIN);
-	assert(sy_lmul(LONG_MAX, 0, NULL) == 0);
-	assert(sy_lmul(LONG_MIN, 0, NULL) == 0);
+
+#if LONG_MAX + LONG_MIN > 0
+	assert(sy_lmul(LONG_MAX, -1, NULL) == LONG_MIN);
+#elif LONG_MAX + LONG_MIN < 0
+	assert(sy_lmul(LONG_MIN, -1, NULL) == LONG_MAX);
+#endif
+
 	assert(sy_lmul(LONG_MAX, LONG_MAX, NULL) == LONG_MAX);
 	assert(sy_lmul(LONG_MAX, LONG_MIN, NULL) == LONG_MIN);
 	assert(sy_lmul(LONG_MIN, LONG_MAX, NULL) == LONG_MIN);
 
 	err = SY_ERROR_NONE;
-	assert(sy_lmul(LONG_MAX, 2, &err) == LONG_MAX);
+	sy_lmul(LONG_MAX, 2, &err);
 	assert(err == SY_ERROR_OVERFLOW);
 
 	err = SY_ERROR_NONE;
-	assert(sy_lmul(LONG_MIN, 2, &err) == LONG_MIN);
+	sy_lmul(LONG_MIN, 2, &err);
+	assert(err == SY_ERROR_UNDERFLOW);
+
+#if LONG_MAX + LONG_MIN > 0
+	err = SY_ERROR_NONE;
+	sy_lmul(LONG_MAX, -1, &err);
+	assert(err == SY_ERROR_UNDERFLOW);
+#elif LONG_MAX + LONG_MIN < 0
+	err = SY_ERROR_NONE;
+	sy_lmul(LONG_MIN, -1, &err);
+	assert(err == SY_ERROR_OVERFLOW);
+#endif
+
+	err = SY_ERROR_NONE;
+	sy_lmul(LONG_MAX, LONG_MAX, &err);
+	assert(err == SY_ERROR_OVERFLOW);
+
+	err = SY_ERROR_NONE;
+	sy_lmul(LONG_MAX, LONG_MIN, &err);
+	assert(err == SY_ERROR_UNDERFLOW);
+
+	err = SY_ERROR_NONE;
+	sy_lmul(LONG_MIN, LONG_MAX, &err);
 	assert(err == SY_ERROR_UNDERFLOW);
 
 	return 0;
