5db4d41a964951ebda572d04babae02701f8a68f
Lucas Hart <lukke100@gmail.com>
2022-01-16 12:18:07 -0500

    Improve the tests for long division

--- tests/t004-long-div.c
+++ tests/t004-long-div.c
@@ -5,5 +5,39 @@
 
 int main(void)
 {
+	enum sy_error err;
+
+	assert(sy_ldiv( 0, 0, NULL) == 0);
+	assert(sy_ldiv( 1, 0, NULL) == LONG_MAX);
+	assert(sy_ldiv(-1, 0, NULL) == LONG_MIN);
+
+#if LONG_MAX + LONG_MIN > 0
+	assert(sy_ldiv(LONG_MAX, -1, NULL) == LONG_MIN);
+#elif LONG_MAX + LONG_MIN < 0
+	assert(sy_ldiv(LONG_MIN, -1, NULL) == LONG_MAX);
+#endif
+
+	err = SY_ERROR_NONE;
+	sy_ldiv(0, 0, &err);
+	assert(err == SY_ERROR_DIVIDE_BY_ZERO);
+
+	err = SY_ERROR_NONE;
+	sy_ldiv(1, 0, &err);
+	assert(err == SY_ERROR_DIVIDE_BY_ZERO);
+
+	err = SY_ERROR_NONE;
+	sy_ldiv(-1, 0, &err);
+	assert(err == SY_ERROR_DIVIDE_BY_ZERO);
+
+#if LONG_MAX + LONG_MIN > 0
+	err = SY_ERROR_NONE;
+	sy_ldiv(LONG_MAX, -1, &err);
+	assert(err == SY_ERROR_UNDERFLOW);
+#elif LONG_MAX + LONG_MIN < 0
+	err = SY_ERROR_NONE;
+	sy_ldiv(LONG_MIN, -1, &err);
+	assert(err == SY_ERROR_OVERFLOW);
+#endif
+
 	return 0;
 }
