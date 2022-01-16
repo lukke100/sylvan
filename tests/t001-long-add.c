4bc37ca000e4783782aaf991a3aad46045c50f5f
Lucas Hart <lukke100@gmail.com>
2022-01-16 11:50:38 -0500

    Take out useless assertions for long add

--- tests/t001-long-add.c
+++ tests/t001-long-add.c
@@ -7,16 +7,15 @@ int main(void)
 {
 	enum sy_error err;
 
-	assert(sy_ladd(LONG_MAX, 1, NULL) == LONG_MAX);
+	assert(sy_ladd(LONG_MAX,  1, NULL) == LONG_MAX);
 	assert(sy_ladd(LONG_MIN, -1, NULL) == LONG_MIN);
-	assert(sy_ladd(LONG_MAX, LONG_MIN, NULL) == LONG_MAX + LONG_MIN);
 
 	err = SY_ERROR_NONE;
-	assert(sy_ladd(LONG_MAX, 1, &err) == LONG_MAX);
+	sy_ladd(LONG_MAX, 1, &err);
 	assert(err == SY_ERROR_OVERFLOW);
 
 	err = SY_ERROR_NONE;
-	assert(sy_ladd(LONG_MIN, -1, &err) == LONG_MIN);
+	sy_ladd(LONG_MIN, -1, &err);
 	assert(err == SY_ERROR_UNDERFLOW);
 
 	return 0;
