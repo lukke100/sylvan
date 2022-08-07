7659dfc7fceb052ea90a4c26968797f45c958342
Lucas Hart <lukke100@gmail.com>
2022-08-07 13:04:48 -0400

    Use bigger numbers in sy_lgcd test

--- tests/t008-long-gcd.c
+++ tests/t008-long-gcd.c
@@ -4,16 +4,16 @@
 
 int main(void)
 {
-	assert(sy_lgcd( 24681,  13566) ==   57);
-	assert(sy_lgcd(-31816,  22116) ==  388);
-	assert(sy_lgcd(-10082, -23643) ==   71);
-	assert(sy_lgcd(-28367, -25381) == 1493);
+	assert(sy_lgcd( 1628292849L,  1509142773L) ==     3);
+	assert(sy_lgcd(-1921961588L,  1062552564L) ==    68);
+	assert(sy_lgcd( 1617517393L, -1067601194L) == 16411);
+	assert(sy_lgcd(-1453988130L, -1680776370L) ==   210);
 
-	assert(sy_lgcd( 21133, 0) == 21133);
-	assert(sy_lgcd(-18370, 0) == 18370);
+	assert(sy_lgcd( 1031745172L, 0) == 1031745172L);
+	assert(sy_lgcd(-1098423335L, 0) == 1098423335L);
 
-	assert(sy_lgcd(0,  19608) == 19608);
-	assert(sy_lgcd(0, -24138) == 24138);
+	assert(sy_lgcd(0,  1711855738L) == 1711855738L);
+	assert(sy_lgcd(0, -1104740303L) == 1104740303L);
 
 	assert(sy_lgcd(0, 0) == 0);
 
