a4604f9adc413582df6b0d8a43bd770490bf1ee0
Lucas Hart <lukke100@gmail.com>
2025-07-17 02:47:52 -0400

    Fix minor copy and paste issues in test t094
    I copied this test from the size_t equivalent, but forgot to clean it.

--- tests/t094-unsigned-div-saturate.c
+++ tests/t094-unsigned-div-saturate.c
@@ -1,8 +1,7 @@
 #include <assert.h>
+#include <limits.h>
 #include <stddef.h>
 #include "sylvan.h"
-
-static const unsigned UINT_MAX = ~(unsigned)0;
 
 int main(void)
 {
@@ -12,12 +11,12 @@ int main(void)
 	assert(sy_udiv_saturate(UINT_MAX, 2, 0) == UINT_MAX);
 
 	assert(sy_udiv_saturate(UINT_MAX, UINT_MAX, UINT_MAX) == UINT_MAX);
-	assert(sy_udiv_saturate(UINT_MAX, UINT_MAX,    1) ==    1);
-	assert(sy_udiv_saturate(UINT_MAX, UINT_MAX,    0) ==    0);
+	assert(sy_udiv_saturate(UINT_MAX, UINT_MAX,        1) ==        1);
+	assert(sy_udiv_saturate(UINT_MAX, UINT_MAX,        0) ==        0);
 
 	assert(sy_udiv_saturate(0, 0, UINT_MAX) == UINT_MAX);
-	assert(sy_udiv_saturate(0, 0,    1) ==    1);
-	assert(sy_udiv_saturate(0, 0,    0) ==    0);
+	assert(sy_udiv_saturate(0, 0,        1) ==        1);
+	assert(sy_udiv_saturate(0, 0,        0) ==        0);
 
 	return 0;
 }
