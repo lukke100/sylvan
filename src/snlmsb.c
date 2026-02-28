6f6ad6f2c774090ae920ed19ee89a7c0dfccb199
Lucas Hart <lukke100@gmail.com>
2026-02-28 08:44:38 -0500

    Break snlmsb's dependency on snlshr

--- src/snlmsb.c
+++ src/snlmsb.c
@@ -1,5 +1,6 @@
 #include "config.h"
-#include <stddef.h>
+#include <limits.h>
+#include <stdlib.h>
 #include "sylvan.h"
 
 static const size_t ZMAX = -1;
@@ -8,13 +9,29 @@ size_t snlmsb(long x, enum sn_error *err)
 {
 	size_t result;
 
+#if LONG_MAX + LONG_MIN >= -1
+	if (x < 0)
+		x = -1L - x;
+
 	result = ZMAX;
 
-	if (x == 0 || x == -1)
+	if (x == 0)
 		sneset(err, SN_ERROR_UNDEFINED);
 
-	for (; x != snlshr(x, 1); x = snlshr(x, 1))
+	for (; x > 0; x >>= 1)
 		++result;
+#else
+	if (x > -1)
+		x = -1L - x;
+
+	result = ZMAX;
+
+	if (x == -1)
+		sneset(err, SN_ERROR_UNDEFINED);
+
+	for (; x < -1; x = snldiv(x, 2, NULL))
+		++result;
+#endif
 
 	return result;
 }
